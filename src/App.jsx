3c4c5cc3ee1605d2ccb9635e9655b5094585fc93
rolomtech <support@rolomtech.com>
2023-01-27 08:30:36 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+function App() {
+  return <div className="bg-primary-500">App</div>;
+}
+
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: colors.blue
+      }
+    },
+  },
+  plugins: [],
+}
