5c317d9873aaceb9aab7d9b1d4bfbcd6e942180e
rolomtech <support@rolomtech.com>
2023-01-29 23:25:37 +0100

    refactored the offer component

--- src/components/Offer.jsx
+++ src/components/Offer.jsx
@@ -4,27 +4,21 @@ import Course from "./Course.jsx";
 
 function Offer() {
   return (
-    <section>
-      <div
-        id="offer"
-        className="w-full relative bg-slate-50 px-7 md:px-16 text-center"
-      >
-        <h1 className="font-bold font-alfa pt-10 text-primary-500">
+    <section
+      id="offer"
+      className="w-full relative bg-slate-50 px-7 md:px-16 text-center"
+    >
+      <div className="relative z-10">
+        <h1 className="font-bold font-alfa pt-10 text-primary-500 uppercase">
           what we offer
         </h1>
-        <div className="max-w-3xl mx-auto mt-5">
-          <p>
-            We offer a variety of programming languages and technologies, our
-            classes are well structure, engaging, and tailored to meet the needs
-            of all learners, whether you're new to programming or have some
-            experience under your belt.
-          </p>
-        </div>
+        <p className="max-w-3xl my-8 text-lg mx-auto mt-5">
+          We offer a variety of programming languages and technologies, our
+          classes are well structure, engaging, and tailored to meet the needs
+          of all learners, whether you're new to programming or have some
+          experience under your belt.
+        </p>
         <div>
-          <div className="absolute -left-10 top-1/2 -translate-y-1/2 w-52 h-60">
-            <img src={Arrow} className="" alt="an arrow design" />
-          </div>
-
           <div className="flex w-full justify-center items-center gap-10">
             <Course
               title="HTML & CSS"
@@ -59,6 +53,11 @@ function Offer() {
           </div>
         </div>
       </div>
+      <img
+        src={Arrow}
+        className="absolute -left-10 top-1/2 -translate-y-1/2 w-52 h-60"
+        alt="an arrow design"
+      />
     </section>
   );
 }
