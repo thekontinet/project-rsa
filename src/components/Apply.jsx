7aba2e81c23e88554ce584f017884ce48bbdcd69
OGHENEMINE <66541923+OGHENEMINE@users.noreply.github.com>
2023-02-23 13:00:14 +0100

    made a correction to the apply component

--- src/components/Apply.jsx
+++ src/components/Apply.jsx
@@ -5,7 +5,7 @@ function Apply({id}) {
   return (
     <section ref={id} className='bg-primary-600 text-center tracking-widest pb-8 text-white px-5 xl:px-96'>
       <h3 className='pt-10'>Ready to start your coding journey? Apply today and take the first step towards a brighter future in tech!</h3>
-      <Link to='/choose-course' className='my-8 button capitalize bg-white hover:bg-orange-500 hover:text-white hover:animate-none transition-all duration-300 ease-in-out animate-breathing font-bold text-black'>click here to apply now</Link>
+      <Link to='/course-application' className='my-8 button capitalize bg-white hover:bg-orange-500 hover:text-white hover:animate-none transition-all duration-300 ease-in-out animate-breathing font-bold text-black'>click here to apply now</Link>
       <p>Don't hesitate to contact us if you have any questions</p>
     </section>
   )
